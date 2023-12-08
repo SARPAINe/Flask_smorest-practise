aaeaf0a20600fab12b818b0a02a0de8f6a0bd5c5
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-12-08 12:21:43 +0600

    completed many to many relationship

--- models/item.py
+++ models/item.py
@@ -13,3 +13,4 @@ class ItemModel(db.Model):
     )
     store = db.relationship("StoreModel", back_populates="items")
     # here items is the name of the property in StoreModel table
+    tags = db.relationship("TagModel", back_populates="items", secondary="items_tags")
