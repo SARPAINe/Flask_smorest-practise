cb8279b9ced81e4a122f8322b65dd2bd8b3dbffb
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-12-15 11:19:26 +0600

    feat(UserModel-UserRegister-UserRegisterSchema): email sent to user upon registration

--- db.py
+++ db.py
@@ -1,3 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db=SQLAlchemy()
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from flask_sqlalchemy.model import Model
+
+    BaseModel = db.make_declarative_base(Model)
+else:
+    BaseModel = db.Model
--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
-from db import db
+from db import db,BaseModel
 
-
-class UserModel(db.Model):
+class UserModel(BaseModel):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String,unique=True,nullable=False,server_default="default@example.com")
     password = db.Column(db.String(256), nullable=False)
